97e8af1d0d480b9fd7eef46fda15fec1fc3b3d3b
Víctor Ruiz Gómez <victorruizgomez642@gmail.com>
2014-07-18 17:31:57 +0200

    Añadida utilidades para el paso de mensajes servidor/cliente

---
+++ module/core/client_utils.lua
@@ -0,0 +1,79 @@
+
+--[[!
+	\file
+	\brief Es un módulo con utilidades para el paso de mensaje cliente/servidor.
+]]
+
+--[[! Esta tabla guarda todas las funciones que puede ser llamadas por el servidor ]]
+__client = {};
+
+
+--[[ Guardaremos los callbacks en una tabla auxiliar ]]
+local __client_callbacks = {};
+
+--[[! Esta tabla sirver para invocar un método del servidor. 
+__server.func_name. Para invocar la función, la sintaxis es: __server.func_name(callback, ...)
+Donde ... son los argumentos que se pasarán a la función del servidor.
+Callback, en caso de error (si la función no existe o bién se lanzó una asercción en el cuerpo de la función en la
+parte del servidor), obtendrá los siguientes argumentos: false, msg (mensaje sobre el error producido).
+En el caso de que se haya invocado correctamente el método, se recibirá lo siguiente: true, ...   Donde ... son los valores
+de retorno de la función.
+ ]] 
+__server = {};
+setmetatable(__server,
+	{
+		__metatable = false,
+		__newindex = function() end,
+		__index = 
+			function(t, index)
+				local serverFunc = index;
+				return 
+					function(funcCallback, ...)
+						assert(type(funcCallback) == "function");
+						-- Registrar el callback para la respuesta del servidor.
+						__client_callbacks[tostring(funcCallback)] = funcCallback;
+						-- invocar método del servidor.
+						triggerServerEvent("onClientCallServerFunction", localPlayer, serverFunc, tostring(funcCallback), ...);
+					end;
+			end
+	});
+	
+--[[ Esta es una tabla auxiliar que accede solamente a las funciones y elementos invocables de la tabla
+__client ]]
+
+local __client_funcs = {};
+setmetatable(__client_funcs, 
+	{
+		__metatable = false,
+		__newindex = function() end,
+		__index = 
+			function(t, index)
+				local value = __client[index];
+				if (type(value) == "function") or 
+				((type(value) == "table") and (getmetatable(value) ~= nil) and (type(getmetatable(value).__call) == "function")) then
+					return value;
+				end
+			end
+	});
+
+local function call_func(funcName, ...)
+	local func = assert(__client_funcs[funcName], "Function \"" .. funcName .. "\" not exists");
+	return func(...);
+end;
+
+
+--[[ Que hacemos cuando el servidor invoca una de las funciones del cliente ? ]]
+addEvent("onServerCallClientFunction", true);
+addEventHandler("onServerCallClientFunction", root,
+	function(func, funcCallback, ...) 
+		triggerServerEvent("onServerCallClientFunctionResponse", localPlayer, funcCallback, pcall(call_func, func, ...));
+	end);
+
+--[[ Que hacemos cuando recibimos la respuesta del servidor ? ]]
+addEvent("onClientCallServerFunctionResponse", true);
+addEventHandler("onClientCallServerFunctionResponse", root,
+	function(funcCallback, ...) 
+		__client_callbacks[funcCallback](...);
+		__client_callbacks[funcCallback] = nil;
+	end);
+	
---
+++ module/core/server_utils.lua
@@ -0,0 +1,91 @@
+
+--[[!
+	\file
+	\brief Es un módulo con utilidades para el paso de mensajes cliente/servidor.
+]]
+
+--[[! Esta es una tabla que contendrá las funciones que los clientes
+podrán invocar remotamente. Se puede realizar lo siguiente para que los clientes
+puedan acceder a cualquier función del servidor: __server = _G ]]
+__server = {};
+
+
+--[[ Tabla auxiliar para los callbacks del servidor ]]
+local __server_callbacks = {};
+
+
+--[[! Esta es una tabla que usará el servidor para invocar remotamente funciones de los clientes ]]
+__client = {};
+setmetatable(__client,
+	{
+		__metatable = false,
+		__index = 
+			function(t, index)
+				assert(isElement(index) and (getElementType(index) == "player"));
+				local client = index;
+				-- Devuelve una tabla auxiliar para acceder a las funciones del cliente.
+				return 
+					setmetatable({},
+						{
+							__metatable = false,
+							__newindex = function() end,
+							__index = 
+								function(t, index)
+									return 
+										function(funcCallback, ...)
+											assert(type(funcCallback) == "function");
+											-- Registramos el callback.
+											__server_callbacks[tostring(funcCallback)] = funcCallback;
+											triggerClientEvent(client, "onServerCallClientFunction", root, index, tostring(funcCallback), ...);
+										end;
+								end
+						});
+			end
+	});
+
+--[[ Esta tabla nos permite acceder únicamente a las funciones incluidas en la tabla
+__server ]]
+local __server_funcs = {};
+setmetatable(__server_funcs,
+	{
+		__metatable = false,
+		__newindex = function() end,
+		__index =
+			function(t, index)
+				local value = __server[index];
+				if (type(value) == "function") or 
+				((type(value) == "table") and (getmetatable(value) ~= nil) and (type(getmetatable(value).__call) == "function")) then
+					return value;
+				end;
+			end
+	});
+	
+local function call_func(funcName, ...)
+	local func = assert(__server_funcs[funcName], "Function \"" .. funcName .. "\" not exists");
+	return func(...);
+end;
+
+
+
+--[[ Que hacemos cuando el cliente quiere invocar una de los métodos del servidor ? ]]
+addEvent("onClientCallServerFunction", true);
+addEventHandler("onClientCallServerFunction", root,
+	function(func, funcCallback, ...)
+		-- El evento fue invocado por un cliente ? ...
+		if client then 
+			-- invocar el método y devolver una respuesta.
+			triggerClientEvent(client, "onClientCallServerFunctionResponse", root, funcCallback, pcall(call_func, func, ...));
+		end;
+	end);
+
+--[[ Que hacemos cuando el cliente nos envía la respuesta después de haber invocado alguno de sus 
+métodos? ]]
+addEvent("onServerCallClientFunctionResponse", true);
+addEventHandler("onServerCallClientFunctionResponse", root,
+	function(funcCallback, ...) 
+		if client then 
+			__server_callbacks[funcCallback](...);
+			__server_callbacks[funcCallback] = nil;
+		end;
+	end);
+
