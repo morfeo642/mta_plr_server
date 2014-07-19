70e75243b54bead8cf5b9aeb743ad9d56a48ab84
Víctor Ruiz Gómez <victorruizgomez642@gmail.com>
2014-07-19 22:01:57 +0200

    Añadido módulo para reemplazar función print

---
+++ module/util/print.lua
@@ -0,0 +1,22 @@
+
+--[[!
+	\file
+	\brief Este es un módulo que permite usar la función print (la reemplaza de la tabla _G), usando la función outputDebugString.
+]]
+
+_G.print = 
+	function(...)
+		local str = "";
+		local args = {...};
+		local i = 1; 
+		if #args > 0 then 
+			while (i < #args) do 
+				str = str .. tostring(args[i]) .. "     ";
+				i = i + 1; 
+			end; 
+			str = str .. args[#args];
+			outputDebugString(str);
+		else
+			outputDebugString("");
+		end;
+	end;
