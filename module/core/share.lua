24de9b048afeeb171e164eaf45603255cf956c46
Víctor Ruiz Gómez <victorruizgomez642@gmail.com>
2014-07-21 22:37:29 +0200

    Ahora se pueden compartir dos tipos de variables: de lectura y lectura y escritura. Tambíen se añaden ejemplos de uso de este módulo

--- module/core/share.lua
+++ module/core/share.lua
@@ -2,20 +2,89 @@
 --[[!
 	\file 
 	\brief Este script permite establecer un protocolo entre diferentes recursos 
-	para compartir variables.
+	para compartir variables e invocar funciones exportadas con mayor facilidad.
+	
+	\code
+	-- Invocar la función foo() del recurso "bar".
+	bar.foo(...);
+	\endcode
+	
+	\code
+	-- Establecer la variable global compartida "x" de "bar" a 50.
+	bar.x = 50; -- solo funciona si la variable x está marcada como pública.
+	\endcode
+	
+	\code
+	-- Como compartir una variable para que los demás recursos puedan tanto escribirla como leerla ? 
+	__share.x = ...;
+	-- O también
+	__share.public.x = ...; -- por defecto, si se indica como __share.x, la variable es pública.
+	\endcode
+	
+	\code
+	-- Para crear variables compartidas protegidas...
+	__share.protected.x = ...;
+	\endcode
 ]]	
 
 
---[[! En esta tabla se añaden funciones y variables que se quieren compartir (lectura y escritura), pero no pueden
-crearse nuevas variables ]]
+--[[! En esta tabla se añaden funciones y variables que se quieren compartir, pero no pueden
+crearse nuevas variables. En la tabla __share.public se almacenan las variables que son de lectura y escritura, y 
+la tabla __share.protected, las variables de solo lectura]]
 __share = {};
+__share.public = {}; 
+__share.protected = {};
+setmetatable(__share,
+	{
+		__metatable = false,
+		__index = 
+			function(t, index) 
+				if t.public[index] ~= nil then 
+					return t.public[index];
+				end;
+				return t.protected[index];
+			end,
+		__newindex = 	
+			function(t, index, value)
+				if value == nil then 
+					rawset(t.public, index, nil);
+					rawset(t.protected, index, nil);
+				else 
+					t.public[index] = value;
+				end;
+			end
+	});
+setmetatable(__share.public, 
+	{
+		__metatable = false,
+		__newindex = 
+			function(t, index, value)
+				if (value ~= nil) and (__share.protected[index] ~= nil) then
+					__share.protected[index] = nil;
+				end;
+				rawset(t, index, value);
+			end
+	});
+setmetatable(__share.protected, 
+	{
+		__metatable = false,
+		__newindex = 
+			function(t, index, value)
+				if (value ~= nil) and (__share.public[index] ~= nil) then
+					__share.public[index] = nil;
+				end;
+				rawset(t, index, value);
+			end
+	});
+
+	
 
 --[[ Para exportar y compartir variables, hay que exportar explicitamente (indicandolo en el archivo meta.xml), las
 funciones __set y __get ]]
 
 function __set(index, value)
-	assert(index and (__share[index] ~= nil));
-	__share[index] = value;
+	assert(index and (__share.public[index] ~= nil) and (__share.protected[index] == nil));
+	__share.public[index] = value;
 end;
 
 function __get(index)
