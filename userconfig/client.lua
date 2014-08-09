4c51cc133bb19950451af7e641ada5b6d3b740f0
Víctor Ruiz Gómez <victorruizgomez642@gmail.com>
2014-08-09 17:06:09 +0200

    Se añade recurso userconfig

---
+++ userconfig/client.lua
@@ -0,0 +1,132 @@
+
+-- Funciones auxiliares.
+
+
+-- Esta función auxiliar carga la configuración local del cliente, desde un fichero XML de un recurso especifico "userconfig.xml".
+local function loadLocalConfigDataFromXMLFile(resource)
+	local function loadLocalConfigDataFromXMLNode(parent) 
+		local configData = {};
+		for _, node in ipairs(xmlNodeGetChildren(parent)) do 
+			local configDataType = xmlNodeGetName(node);
+			local configDataName, configDataValue = xmlNodeGetAttribute(node, "name"), xmlNodeGetAttribute(node, "value");
+			if ((configDataType == "boolean") or (configDataType == "number") or (configDataType == "string") or (configDataType == "table")) then 
+				if configDataType == "number" then
+					configDataValue = tonumber(configDataValue);
+				elseif configDataType == "boolean" then 
+					if configDataValue == "true" then 
+						configDataValue = true;
+					elseif configDataValue == "false" then 
+						configDataValue = false;
+					else
+						configDataValue = nil;
+					end;
+				elseif configDataType == "table" then 
+					configDataValue = table.fromJSON(configDataValue);
+					if not configDataValue then 
+						configDataValue = nil;
+					end;
+				end;
+				
+				if configDataValue ~= nil then 
+					configData[configDataName] = configDataValue;
+				end;	
+			end;	
+		end;
+		return configData;
+	end;
+
+	local file = xmlLoadFile(":" .. getResourceName(resource) .. "/userconfig.xml"); 
+	if not file then 
+		return {}; 
+	end;
+	local configData = loadLocalConfigDataFromXMLNode(file);
+	xmlUnloadFile(file);
+	return configData;
+end; 
+
+-- Esta función guarda la configuración local del usuario en un fichero XML.
+local function saveLocalConfigDataToXMLFile(resource, configData)
+	local function saveLocalConfigDataToXMLNode(parent, configData)
+		for configDataName, configDataValue in pairs(configData) do 
+			local configDataType = type(configDataValue);
+			if (configDataType == "number") or (configDataType == "boolean") then 
+				configDataValue = tostring(configDataValue);
+			elseif configDataType == "table" then 
+				configDataValue = table.toJSON(configDataValue);
+			end;
+			local node = xmlCreateChild(parent, configDataType);
+			xmlNodeSetAttribute(node, "name", configDataName);
+			xmlNodeSetAttribute(node, "value", configDataValue);
+		end;
+	end;
+	local file = xmlCreateFile(":" .. getResourceName(resource) .. "/userconfig.xml", "userconfig");
+	saveLocalConfigDataToXMLNode(file, configData);
+	xmlSaveFile(file);
+end;	
+
+
+-- Tablas auxiliares
+local configData = {};
+local configDataChanged = {};
+
+-- Funciones exportadas.
+
+--[[!
+	Establece el valor de una variable de configuración local del cliente. 
+	@param configDataName Es el nombre del dato.
+	@param configDataValue Es el nuevo valor del dato. Nil para borrarlo. Puede ser un valor booleano, numero, string o tabla (siempre 
+	que sea convertible a string mediante table.toJSON)
+]]
+function setClientLocalConfigData(configDataName, configDataValue)
+	checkArgumentType("setClientLocalConfigData", 2, configDataName, 1, "string");
+	checkOptionalArgumentType("setClientLocalConfigData", 2, configDataValue, 2, "string", "number", "boolean", "table");
+	localizedAssert((type(configDataValue) ~= "table") or table.toJSON(configDataValue));
+	
+	-- si la configuración local no se ha cargado todavía, leer del fichero XML del recurso.
+	if not configData[sourceResource] then 
+		configData[sourceResource] = loadLocalConfigDataFromXMLFile(sourceResource);
+	end;	
+	if configData[sourceResource][configDataName] ~= configDataValue then 
+		configData[sourceResource][configDataName] = configDataValue; 
+		configDataChanged[sourceResource] = true;
+	end;
+end; 
+
+
+--[[!
+	@return Devuelve una variable de configuración local del cliente. (Se guarda localmente, y no en el 
+	servidor). Devuelve nil si esa variable no existe.
+	@param configDataName Es el nombre de la variable de configuración. 
+	@note Las variables de configuración de un recurso son independientes de las variables de configuración de otro
+	recurso, por lo que no hay colisiones entre nombres.
+]]
+function getClientLocalConfigData(configDataName)
+	checkArgumentType("getClientLocalConfigData", 2, configDataName, 1, "string");
+
+	-- si la configuración local no se ha cargado todavía, leer del fichero XML del recurso.
+	if not configData[sourceResource] then 
+		configData[sourceResource] = loadLocalConfigDataFromXMLFile(sourceResource);
+	end;
+	return configData[sourceResource][configDataName]; 
+end;
+
+
+
+-- Eventos.
+
+addEventHandler("onClientResourceStart", resourceRoot,	
+	function() 
+		-- cargamos los módulos necesarios.
+		loadModule("util/checkutils", _G);
+		loadModule("util/tableutils", _G);
+	end);
+	
+addEventHandler("onClientResourceStop", root,
+	function(resource) 
+		-- guardar la configuración si ha sido modificada.
+		if configDataChanged[resource] then 
+			configDataChanged[resource] = nil;
+			saveLocalConfigDataToXMLFile(resource, configData[resource]);
+			configData[resource] = nil;
+		end;
+	end);	
---
+++ userconfig/server.lua
@@ -0,0 +1,75 @@
+
+-- Funciones auxiliares.
+local function _getPlayerConfigData(sourceResource, player, configDataName)
+	local configDataValue = getAccountData(getPlayerAccount(player), getResourceName(sourceResource) .. ":" .. configDataName);
+	if not configDataValue then 	
+		return nil;
+	end;
+	local dataType;
+	dataType, configDataValue = configDataValue:match("(%a)(.+)");
+	
+	-- que tipo de dato es ? 
+	if dataType == "n" then 
+		configDataValue = tonumber(configDataValue);
+	elseif dataType == "b" then 
+		configDataValue = (configDataValue == "true");
+	elseif dataType == "t" then
+		configDataValue = table.fromJSON(configDataValue);
+	end;
+	return configDataValue;
+end; 
+
+--[[!
+	Establece el valor de una variable en la configuración del usuario.
+	Esta configuración se guardará en la cuenta del usuario, y por tanto, no será
+	accesible directamente por el cliente. 
+	@param player Es el jugador del que se quiere cambiar su configuración.
+	@param configDataName Es el nombre de la variable de configuración
+	@param configDataValue Es el nuevo valor de la variable, nil para borrar la variable de la cuenta.
+	El valor puede ser un número, booleano, string o una tabla que sea convertible a string mediante table.toJSON
+	
+	@note La configuración será privada para cada recurso para evitar conflictos de nombres.
+]]
+function setPlayerConfigData(player, configDataName, configDataValue)
+	checkArgumentsTypes("setPlayerConfigData", 2, 1, player, "player", configDataName, "string");
+	checkOptionalArgumentType("setPlayerConfigData", 2, configDataValue, 3, "boolean", "string", "number", "table");
+	local preffix;
+	if configDataValue ~= nil then preffix = type(configDataValue):sub(1,1); end;
+
+	if type(configDataValue) == "table" then 	
+		local validTable;
+		validTable, configDataValue = pcall(table.toJSON, configDataValue);
+		localizedAssert(validTable, "Bad table passed to setPlayerConfigData", 2);
+	elseif (type(configDataValue) == "number") or (type(configDataValue) == "boolean") then 
+		configDataValue = tostring(configDataValue);
+	elseif type(configDataValue) ~= "string" then 
+		configDataValue = false;
+	end;
+	configDataName = getResourceName(sourceResource) .. ":" .. configDataName;
+		
+	if configDataValue then 
+		setAccountData(getPlayerAccount(player), configDataName, preffix .. configDataValue);
+	else
+		setAccountData(getPlayerAccount(player), configDataName, false);
+	end;	
+end;
+
+--[[!
+	@return Devuelve el valor de una variable de configuración de un jugador (nil si no existe)
+	@param player Es el jugador
+	@param configDataName Es el nombre de la variable de configuración.
+]]
+function getPlayerConfigData(player, configDataName)
+	checkArgumentsTypes("setPlayerConfigData", 2, 1, player, "player", configDataName, "string");
+	
+	return _getPlayerConfigData(sourceResource, player, configDataName);
+end;
+
+
+addEventHandler("onResourceStart", resourceRoot,
+	function() 
+		-- cargamos los módulos necesarios
+		loadModule("util/checkutils", _G);
+		loadModule("util/tableutils", _G);
+	end);
+	
