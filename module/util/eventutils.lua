907af3730d216d81f1fb6e70164628cae7cc3816
Víctor Ruiz Gómez <victorruizgomez642@gmail.com>
2015-01-24 20:49:32 +0100

    Arreglado algunos errores en el código y añadida comprobación de los argumentos a las funciones addAutoremovableEventHandler y addCancelerEventHandler

--- module/util/eventutils.lua
+++ module/util/eventutils.lua
@@ -7,19 +7,22 @@
 ]]
 
 
+loadModule("util/checkutils");
+
 --[[!
 Crea un manejador de evento que solo se lanza cuando el evento en cuestión se genera
 (sobre el elemento al que está vinculado), una única vez, y luego se elimina así mismo.
 Los parámetros de está función son los mismos que para la función addEventHandler
 ]]
 function addAutoremovableEventHandler(eventName, attachedTo, handlerFunction, getPropagated, priority)
-	assert(type(handlerFunction) == "function");
+	checkArgumentType("addAutoremovableEventHandler", 2, eventName, 1, "string");
+	checkIsElement("addAutoremovableEventHandler", 2, attachedTo, 2);
+	checkArgumentType("addAutoremovableEventHandler", 2, handlerFunction, 3, "function");
+	checkOptionalArgumentsTypes("addAutoremovableEventHandler", 2, 4, getPropagated, "boolean", priority, "string");
 	local function autoRemovableEventHandler(...)
-		local success, msg = pcall(handlerFunction, ...);
 		-- eliminar el manejador.
-		removeEventHandler(eventName, attachedTo, handlerFunction);
-		-- propagar el error si es que hubo alguno en el manejador.
-		if not success then error(msg); end; 
+		removeEventHandler(eventName, attachedTo, autoRemovableEventHandler);
+		handlerFunction(...);
 	end;
 	return addEventHandler(eventName, attachedTo, autoRemovableEventHandler, getPropagated, priority);
 end;	
@@ -32,10 +35,16 @@ Crea un manejador que siempre cancela el evento que está manejando mediante el 
 @param getPropagated Indica si el evento debe propagarse hacía arriba y abajo del árbol de elementos
 @param priority Es la pioridad de este manejador para se invocado
 @param autoRemovable Si es true, el manejador solo cancelará el evento una vez y luego se eliminará. Si es false,
-el evento se cancela continuamente.
+el evento se cancela continuamente. Por defecto, false.
 @param reason Es la razón de porque el evento es cancelado, por defecto ninguna
 ]]
 function addCancellerEventHandler(eventName, attachedTo, getPropagated, priority, autoRemovable, reason)
+	checkArgumentType("addCancellerEventHandler", 2, eventName, 1, "string");
+	checkIsElement("addCancellerEventHandler", 2, attachedTo, 2);
+	checkOptionalArgumentsTypes("addCancellerEventHandler", 2, 3, getPropagated, "boolean", priority, "string",
+			autoRemovable, "boolean", reason, "string");
+	autoRemovable, reason = parseOptionalArguments(autoRemovable, false, reason, "");
+	
 	local function cancellerEventHandler() 
 		cancelEvent(true, reason);
 	end;
