19f3c22fb23c3a4ba0648ca492693c04d819c88f
Victor <victorruizgomez642@gmail.com>
2015-02-17 11:49:27 +0100

    Mejorado el módulo util/class

--- module/util/class.lua
+++ module/util/class.lua
@@ -27,7 +27,7 @@
 	\endcode
 	
 	\code                                                                                 
-	-- Por último, si especificamos un inicializador de clase, tenemos que preocuparnos de inicializar la
+	-- Si especificamos un inicializador de clase, tenemos que preocuparnos de inicializar la
 	-- superclase.
 	bar = class(foo);
 	function bar:init()
@@ -35,21 +35,41 @@
 		print("inicializando instancia de bar");
 	end;
 	\endcode
+	-- Para comprobar si una variable es una clase, se puede usar la función isvalidclass
+	\code
+	bar = class();
+	print(isvalidclass(bar));
+	\endcode
+	
+	-- Podemos comprobar si un objeto es una instanciación de una clase.
+	\code 
+	bar = class();
+	instance = bar();
+	print(instance:isinstanceof(bar));
+	\endcode
 ]]
 
 loadModule("util/assertutils");
 
 function class(BaseClass)
-	localizedAssert((type(BaseClass) == "table") or (BaseClass == nil), "Invalid base class", 2);
+	localizedAssert((BaseClass == nil) or isvalidclass(BaseClass), "Invalid base class", 2);
 	local newClass = {};
 	local newClassMetatable = {};
 	
 	newClass.__index = newClass;
 	
+	-- Esto es para proteger a las instancias de la nueva clase, que no se les pueda
+	-- asignar nuevas metatablas
+	newClass.__metatable = newClass;
+	
 	if BaseClass then  
 		newClassMetatable.__index = BaseClass;
 		newClass.__base = BaseClass;
 	end;
+	
+	-- Proteger a la nueva clase para que no se pueda establecer una nueva 
+	-- metatabla y al mismo tiempo, que getmetatable(newClass) == newClassMetatable
+	newClassMetatable.__metatable = newClassMetatable;
 	
 	-- "Constructor" de clase.
 	newClassMetatable.__call =
@@ -92,7 +112,7 @@ function class(BaseClass)
 			-- no hace falta invocar al constructor.
 			return other;
 		end;
-	-- Comprobar si un objeto hereda de una clase.
+	-- Comprobar si un objeto es una instancia de una clase heredada por esta.
 	newClass.isinstanceof =
 		function(this, otherClass)
 			localizedAssert(type(otherClass) == "table", "Invalid class", 2);
@@ -107,3 +127,80 @@ function class(BaseClass)
 	return newClass;
 end;
 
+
+--[[!
+@return Devuelve un valor booleano indicando si la variable pasada como parámetro es 
+una clase válida (instanciable, inicializable y clonable)
+]]
+function isvalidclass(class)
+	return (type(class)=="table") and getmetatable(class) and 
+		(type(getmetatable(class).__call)=="function") and (type(class.init)=="function") and
+		(type(class.super)=="function") and (type(class.clone)=="function") and
+		(type(class.isinstanceof)=="function");
+end;
+
+--[[!
+@return Devuelve un valor booleano indicando si la variable pasada como parámetro es 
+un objeto válido; Es una instancia de una clase válida.
+]]
+function isvalidobject(object)
+	return getmetatable(object) and isvalidclass(getmetatable(object));
+end;
+
+--[[!
+@return Devuelve un valor booleano indicando si este objeto es un objeto válido y es una 
+instancia de la clase indicada o de un clase que hereda de esta.
+]]
+function isinstanceof(object, class)
+	return isvalidobject(object) and object:isinstanceof(class);
+end;
+
+
+--[[!
+Comprueba si un valor hace referencia a una clase. 
+@param funcname Es el nombre de la función.
+@param level Especifíca la posición del error en la pila de llamadas
+@param value Es la variable que se quiere verificar si es una clase valida.
+@param argnum Es el cardinal del argumento
+]]
+function checkIsClass(funcname, level, value, argnum)
+	localizedAssert((type(funcname) == "string") and (type(level) == "number") and (type(argnum) == "number") and (argnum > 0),
+		"Bad arguments to \"checkIsClass\"", level);
+	if not isvalidclass(value) then 
+		error("Argument mismatch in function \"" .. funcname .. "\" at argument " .. argnum .. "; class expected, but got " .. tostring(value), level+1);
+	end;
+end;
+
+--[[!
+Comprueba si un valor hace referencia a una instancia de una clase.
+@param funcname Es el nombre de la función.
+@param level Especifíca la posición del error en la pila de llamadas
+@param value Es la variable que se quiere verificar si es un objeto válido.
+@param argnum Es el cardinal del argumento
+]]
+function checkIsObject(funcname, level, value, argnum)
+	localizedAssert((type(funcname) == "string") and (type(level) == "number") and (type(argnum) == "number") and (argnum > 0),
+		"Bad arguments to \"checkIsObject\"", level);
+	if not isvalidobject(value) then 
+		error("Argument mismatch in function \"" .. funcname .. "\" at argument " .. argnum .. "; object expected, but got " .. tostring(value), level+1);
+	end;
+end;
+
+--[[!
+Comprueba si un valor hace referencia a una instancia de la clase indicada, o es una 
+instancia de una clase que hereda de esta.
+@param funcname Es el nombre de la función.
+@param level Especifíca la posición del error en la pila de llamadas
+@param value Es la variable que se quiere verificar si es un objeto válido y es una instancia de la
+clase.
+@param argnum Es el cardinal del argumento
+@param class Es la clase.
+@param classname Es el nombre de la clase (para añadir información adicional en caso de error)
+]]
+function checkIsInstanceOf(funcname, level, value, argnum, class, classname)
+	localizedAssert((type(funcname) == "string") and (type(level) == "number") and (type(argnum) == "number") and (argnum > 0) and isvalidclass(class) and (type(classname)  == "string"),
+		"Bad arguments to \"checkIsInstanceOf\"", level);
+	if not isinstanceof(value, class) then 
+		error("Argument mismatch in function \"" .. funcname .. "\" at argument " .. argnum .. "; an instance of " .. classname .. " class expected, but got " .. tostring(value), level+1);
+	end;
+end;
